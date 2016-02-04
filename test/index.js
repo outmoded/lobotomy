07a2e5998ef5d02c81492c9e467dbc495da300cc
Eran Hammer <eran@hammer.io>
2016-02-04 02:08:07 -0800

    1.0.0

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 
 // Load modules
 
+const Boom = require('boom');
 const Code = require('code');
 const Hapi = require('hapi');
 const Lab = require('lab');
@@ -117,6 +118,180 @@ describe('Lobotomy', () => {
                 expect(res.statusCode).to.equal(200);
                 expect(res.result).to.equal(1);
                 done();
+            });
+        });
+    });
+
+    it('invokes function handler', (done) => {
+
+        const server = new Hapi.Server();
+        server.connection();
+
+        server.register(Lobotomy, (err) => {
+
+            expect(err).to.not.exist();
+
+            server.route({
+                path: '/',
+                method: 'GET',
+                handler: function (request, reply) {
+
+                    return reply(1);
+                }
+            });
+
+            const handler = function (request, reply) {
+
+                return reply(2);
+            };
+
+            server.lobotomize({ path: '/', handler: handler });
+
+            server.inject('/', (res) => {
+
+                expect(res.statusCode).to.equal(200);
+                expect(res.result).to.equal(2);
+                done();
+            });
+        });
+    });
+
+    it('returns error', (done) => {
+
+        const server = new Hapi.Server();
+        server.connection();
+
+        server.register(Lobotomy, (err) => {
+
+            expect(err).to.not.exist();
+
+            server.route({
+                path: '/',
+                method: 'GET',
+                handler: function (request, reply) {
+
+                    return reply(1);
+                }
+            });
+
+            server.lobotomize({ path: '/', handler: Boom.badRequest('kaboom') });
+
+            server.inject('/', (res) => {
+
+                expect(res.statusCode).to.equal(400);
+                expect(res.result.message).to.equal('kaboom');
+                done();
+            });
+        });
+    });
+
+    it('overrides route once', (done) => {
+
+        const server = new Hapi.Server();
+        server.connection();
+
+        server.register(Lobotomy, (err) => {
+
+            expect(err).to.not.exist();
+
+            server.route({
+                path: '/',
+                method: 'GET',
+                handler: function (request, reply) {
+
+                    return reply(1);
+                }
+            });
+
+            server.lobotomize({ path: '/', handler: { payload: 2 }, once: true });
+
+            server.inject('/', (res1) => {
+
+                expect(res1.statusCode).to.equal(200);
+                expect(res1.result).to.equal(2);
+
+                server.inject('/', (res2) => {
+
+                    expect(res2.statusCode).to.equal(200);
+                    expect(res2.result).to.equal(1);
+                    done();
+                });
+            });
+        });
+    });
+
+    it('overrides route after count', (done) => {
+
+        const server = new Hapi.Server();
+        server.connection();
+
+        server.register(Lobotomy, (err) => {
+
+            expect(err).to.not.exist();
+
+            server.route({
+                path: '/',
+                method: 'GET',
+                handler: function (request, reply) {
+
+                    return reply(1);
+                }
+            });
+
+            server.lobotomize({ path: '/', handler: { payload: 2 }, after: 1 });
+
+            server.inject('/', (res1) => {
+
+                expect(res1.statusCode).to.equal(200);
+                expect(res1.result).to.equal(1);
+
+                server.inject('/', (res2) => {
+
+                    expect(res2.statusCode).to.equal(200);
+                    expect(res2.result).to.equal(2);
+                    done();
+                });
+            });
+        });
+    });
+
+    it('overrides route based on filter', (done) => {
+
+        const server = new Hapi.Server();
+        server.connection();
+
+        server.register(Lobotomy, (err) => {
+
+            expect(err).to.not.exist();
+
+            server.route({
+                path: '/',
+                method: 'GET',
+                handler: function (request, reply) {
+
+                    return reply(1);
+                }
+            });
+
+            let count = 0;
+            const filter = function (request, next) {
+
+                return next(++count > 1);
+            };
+
+            server.lobotomize({ path: '/', handler: { payload: 2 }, filter: filter });
+
+            server.inject('/', (res1) => {
+
+                expect(res1.statusCode).to.equal(200);
+                expect(res1.result).to.equal(1);
+
+                server.inject('/', (res2) => {
+
+                    expect(res2.statusCode).to.equal(200);
+                    expect(res2.result).to.equal(2);
+                    done();
+                });
             });
         });
     });
